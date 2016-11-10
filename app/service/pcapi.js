3115490d1911300a5ebf6600826fd6863823bdef
Jose Lloret <jose.lloret@ed.ac.uk>
2016-11-10 14:10:42 +0000

    pcapi third-party library removed

--- app/service/pcapi.js
+++ app/service/pcapi.js
@@ -2,44 +2,37 @@
 	'use strict';
 	var angular = window.angular;
 	angular.module('service')
-		.service('pcapiService',['$q','$log','__ENV',function($q,$log,__ENV){
-			var pcapi = window.pcapi;
-			function init(){
-				pcapi.init({
-            		url: __ENV.pcapi.baseUrl,
-           		 	version: __ENV.pcapi.version
-       			});
-			}
+		.service('pcapiService',['$q','$http','$log','__ENV',function($q,$http,$log,__ENV){
 			this.getEditor = function(sid){
+				//http://localhost/1.3/pcapi/editors/local/00000000-0000-0000-0000-000000000000/survey_test.json
 				if(typeof sid !== 'string')
 					throw new TypeError('A string parameter is expected for sid');
 				var deferred = $q.defer();
-				pcapi.getEditor({remoteDir: 'editors', 
-					userId: __ENV.pcapi.userId,
-					item: sid+'.json'}
-				).then(function(data){
-						if(data.error){
-							deferred.reject(data);
-						}
-						else
-							deferred.resolve(data);
-					},
-					function(error){
-						deferred.reject(error);
-					}
-				);
+				$http({method:'GET',url:__ENV.pcapi.baseUrl+'/'+
+					__ENV.pcapi.version+'/pcapi/editors/local/'+__ENV.pcapi.userId+'/'+sid+'.json'})
+				.then(function(success){
+					if(success.data.error)
+						deferred.reject(success.data);
+					else
+						deferred.resolve(success.data);
+
+				},function(error){
+					deferred.reject(error);
+				});
 				return deferred.promise;
 			};
 			this.getEditorAssetsBaseURL = function(sid){
+				//http://localhost/1.3/pcapi/editors/local/00000000-0000-0000-0000-000000000000/survey_test
 				if(typeof sid !== 'string')
 					throw new TypeError('A string parameter is expected for sid');
-				return pcapi.buildUserUrl(__ENV.pcapi.userId,'editors',sid);
+				return __ENV.pcapi.baseUrl+'/'+
+					__ENV.pcapi.version+'/pcapi/editors/local/'+__ENV.pcapi.userId+'/'+sid;
 			};
 			this.getEditorAssetURL = function(sid,src){
+				//http://localhost/1.3/pcapi/editors/local/00000000-0000-0000-0000-000000000000/survey_test/cat.jpg
 				if(typeof src !== 'string')
 					throw new TypeError('A string parameter is expected for src');
 				return this.getEditorAssetsBaseURL(sid)+'/'+src;
 			};
-			init();
 		}]);
 })(this.window);
